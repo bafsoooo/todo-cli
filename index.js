c68fcd133b636808ad6501def187272305f976a6
Boufis <boufisabderrahmane@gmail.com>
2024-09-06 18:54:06 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,138 @@
+#!/usr/bin/env node
+console.log("index.js is running");
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+const todo = require('./todo');
+
+//command line arguments
+const argv = yargs
+	.command('addTodo', 'Add a new todo', {
+		title: {
+			describe: 'Title of todo',
+			demandOption: true,
+			type: 'string'
+		}
+	})
+	.command('deleteTodo', 'Delete a todo', {
+        title: {
+            describe: 'Title of todo',
+            demandOption: true,
+            type: 'string'
+        }
+    })
+	.command('updateTodo', 'Update a todo', {
+        id: {
+            describe: 'ID of todo',
+            demandOption: true,
+            type: 'string'
+        },
+        title: {
+            describe: 'New title of todo',
+            demandOption: true,
+            type: 'string'
+        }
+    })
+	.command('readTodo', 'Read a todo', {
+        id: {
+            describe: 'ID of todo',
+            demandOption: true,
+            type: 'string'
+        }
+    })
+	.command('listTodos', 'List all todos')
+	.command('listTodosDone', 'List all todos done')
+	.command('listTodosInProgress', 'List all todos In Progress')
+	.command('listTodosNotDone', 'List all todos Not done')
+	.command('mark-in-progress', ' Marking a task as in progress',{
+		id: {
+            describe: 'ID of todo',
+            demandOption: true,
+            type: 'string'
+        }
+	})
+	.command('mark-done', 'Mark a todo as done', {
+        id: {
+            describe: 'ID of todo',
+            demandOption: true,
+            type: 'string'
+        }
+    })
+	.help()
+	.argv;
+
+const command = argv._[0];
+
+console.log('Running Command is :', command);
+
+//handling commands
+if (command === 'addTodo') { //addTodo
+	todo.addTodo(argv.title);
+} 
+else if (command === 'deleteTodo') { //deleteTodo
+    let todoDeleted = todo.deleteTodoByTitle(argv.title);
+    let message = todoDeleted ?
+        'Todo was deleted' : 'Todo cannot be found';
+    console.log(message);
+}
+else if (command === 'readTodo') {	//readTodo
+	let task = todo.readTodo(argv.id);
+	if (task) {
+		console.log('yahoo! The todo was found.');
+		todo.logTodo(task);
+	} else {
+		console.log('Oops! The todo was not found.');
+	}
+} 
+else if (command === 'updateTodo') { //updateTodo
+    let task = todo.updateTodo(argv.id, argv.title);
+    if (task) {
+        console.log('The todo was updated.');
+        todo.logTodo(task);
+    } else {
+        console.log('Oops! The todo was not found.');
+    }
+}
+else if (command === 'mark-in-progress') { //mark-in-progress
+	let task = todo.markInProgress(argv.id);
+	if (task) {
+		console.log(`${task} was marked as in progress.`);
+		todo.logTodo(task);
+	} else {
+		console.log('Oops! The todo was not found.');
+	}
+}
+else if (command === 'mark-done') { //mark-done
+	let task = todo.markDone(argv.id);
+	if (task) {
+		console.log(`${task} was marked as done.`);
+	} else {
+		console.log('Oops! The todo was not found.');
+	}
+}  
+else if (command === 'listTodos') { //listTodos
+    let allTodos = todo.listTodos();
+    console.log(`${allTodos.length} tasks available`);
+    allTodos.forEach((task) => todo.logTodo(task)); // Corrected line
+}  
+else if (command === 'listTodosDone') { //listTodosDone
+	let allTodosDone = todo.listTodosDone();
+	console.log(`${allTodosDone.length} tasks available`);
+	allTodosDone.forEach((task) => todo.logTodo(task));
+}
+else if (command === 'listTodosInProgress') { //listTodosInProgress
+	let allTodosInProgress = todo.listTodosInProgress();
+	console.log(`${allTodosInProgress.length} tasks available`);
+	allTodosInProgress.forEach((task) => todo.logTodo(task));
+}
+else if (command === 'listTodosNotDone') { //listTodosNotDone
+	let allTodosNotDone = todo.listTodosNotDone();
+	console.log(`${allTodosNotDone.length} tasks available`);
+	allTodosNotDone.forEach((task) => todo.logTodo(task));
+}
+else {
+	console.log('It is a Invalid command.');
+}
+
+
---
+++ todo.js
@@ -0,0 +1,134 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const todosFilePath = 'todos.json';
+
+//Add a todo item
+const addTodo = (title) => {
+    const todos = fetchTodos();
+    const id = _.uniqueId(); 
+	const status = 'not done';
+	const createdAt = new Date();
+    todos.push({ id, title ,status, createdAt});
+    saveTodos(todos);
+    console.log('Todo added:', { id, title });
+};
+
+// Delete a todo item
+const deleteTodoByTitle = (title) => {
+    let todos = fetchTodos();
+    let filteredTodos = todos.filter(
+        (todo) => todo.title !== title
+    );
+    saveTodos(filteredTodos);
+
+    return todos.length !== filteredTodos.length;
+};
+
+
+//Read a todo item
+const readTodo = (id) => {
+	let todos = fetchTodos();
+	return todos.find((todo) => todo.id === id);
+};
+
+//Update a todo item
+const updateTodo = (id, newTitle) => {
+    let todos = fetchTodos();
+    let todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.title = newTitle;
+        todo.updatedAt = new Date().toISOString();
+        saveTodos(todos);
+        return todo;
+    }
+    return null;
+};
+//List all todo items
+const listTodos = () => {
+	return fetchTodos();
+};
+
+//List all Done task 
+const listTodosDone = () => {
+	let todos = fetchTodos();
+	let doneTodos = todos.filter(
+		(todo) => todo.status === 'done');
+	return doneTodos;
+};
+
+//List all in progress task
+const listTodosInProgress = () => {
+	let todos = fetchTodos();
+	let doneTodos = todos.filter(
+		(todo) => todo.status === 'in progress');
+	return doneTodos;
+}; 
+
+//List all Done task 
+const listTodosNotDone = () => {
+	let todos = fetchTodos();
+	let doneTodos = todos.filter(
+		(todo) => todo.status === 'not done');
+	return doneTodos;
+};
+
+//Mark a todo item as in progress
+const markInProgress = (id) => {
+	let todos = fetchTodos();
+    let todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.status = 'in proress';
+        saveTodos(todos);
+        return todo.title;
+    }
+    return null;
+};
+
+//Mark a todo item as done
+const markDone = (id) => {
+    let todos = fetchTodos();
+    let todo = todos.find((todo) => todo.id === id);
+    if (todo) {
+        todo.status = 'done';
+        saveTodos(todos);
+        return todo.title;
+    }
+    return null;
+};
+
+//Utility functions
+const fetchTodos = () => {
+	try {
+        const dataBuffer = fs.readFileSync(todosFilePath);
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+};
+
+const saveTodos = (todos) => {
+    const dataJSON = JSON.stringify(todos);
+    fs.writeFileSync(todosFilePath, dataJSON);
+};
+
+const logTodo = (todo) => {
+	console.log('## ---## --- ##');
+	console.log(`${todo.title}`);
+};
+
+// Exporting function
+module.exports = {
+	addTodo,
+	deleteTodoByTitle,
+	readTodo,
+	updateTodo,
+	listTodos,
+	listTodosInProgress,
+	listTodosDone,
+	listTodosNotDone,
+	markInProgress,
+	markDone,
+	logTodo
+};
